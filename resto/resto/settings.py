7ba559808c7277702aa9d1c739f1e6582ab5e826
Ndombi-Gnele Franck <allanfranckgnele@gmail.com>
2026-04-17 05:27:37 +0200

    mise à j

--- resto/resto/settings.py
+++ resto/resto/settings.py
@@ -80,15 +80,19 @@ WSGI_APPLICATION = 'resto.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = dj_database_url.parse(os.environ.get('DATABASE_URL', ''), conn_max_age=600)
-if not DATABASES:
+database_url = os.environ.get("DATABASE_URL")
+
+if database_url:
     DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
+        "default": dj_database_url.parse(database_url, conn_max_age=600)
+    }
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
         }
     }
-
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
 
